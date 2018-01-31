103b2cb5bf52761958bbc331b829f3d0244e450f
kumo <kumotodeas@hotmail.com>
2018-01-30 19:27:22 -0600

    class77 bluej

---
+++ gitwork/src/Clase77ObjetConBlueJ/BlueJ.java
@@ -0,0 +1,8 @@
+/*======================BlueJ y Java==================
+ * -Es un software creado para iniciarnos en la programacion orientada a objetos POO
+ * -Nos facilita ver graficamente la cracion de nuestros objetos y como se va modificando conforme
+ * ejecutamos nuestro codigo.
+ * -Sin embargo, solo es una emulacion, no es un ambiente real de  programacion, es una herramienta
+ * software para aclarar varios conceptos en java.
+ * 
+ * */
