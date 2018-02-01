8c45f4cdfc5b7e9f701fbde847da8887b5b38c02
ElyPus <ElyPus@ElyPus-PC>
2018-01-31 21:21:09 -0600

    clase 87 Matrices

---
+++ gitwork/src/MtricesManejo/ExtraerElementMatriz.java
@@ -0,0 +1,22 @@
+package MtricesManejo;
+/*=================================EXTRAER ELEMENTOS DE UNA MATRIZ===========================
+ * para leer o extraer los elementos de una matriz solo es necesario poner el nombre de la matriz e indicar 
+ * los indices del elemento requerido. nombreMatriz [ren][col]; -> variableReceptora = nombreMatriz [ren][col];
+ * Ejemplos de tipo int(primitivos)
+ * int j = enteros [0][0]; Se extrae el elemento del indice 0-0
+ * int i = enteros [1][0]; Se extrae el elemento del indice 1-0
+ * 
+ * Ejemplos de tipo Object:
+ * Persona p1 = personas[0][0]; Donde se inica con la clase a la que pertenece la matriz, seguida de 
+ * la variable receptora y se iguala con el nombre de la matriz y los indices solicitados.
+ * Persona p2 = personas[1][0]; 
+ * 
+ * String nombres1 = nombres[0][0];
+ * String nombres2 = nombres[1][1];	
+ * 
+ * 
+ * NOTA [][] = celda, los 2 indices componen una celda.
+ * */
+public class ExtraerElementMatriz {
+
+}
---
+++ gitwork/src/MtricesManejo/IniciarInstanciarMatrices.java
@@ -0,0 +1,32 @@
+package MtricesManejo;
+/*=================================INSTANCIAMIENTO DE MATRICEZ=============
+ * SINTAXIS: Es mu parecida a la inicializacion de un array de objetos.
+ * 		nombreMatriz = new tipo [renGlones][colUmnas];
+ * Al delcarar e iniciar la matriz debemos de colocar el maximo de renglones y columnas que tendra la matriz
+ * --Nota:Los array y las Matricez decienden de la clase object. 
+ * */
+public class IniciarInstanciarMatrices {
+	
+	public static void main(String[] args) {
+		//Ejemplo de matrices de tipo primitivo
+		int enteros [ ];
+		enteros = new int[2][4];
+		banderas = new boolean [3][4];
+		//Ejemplo de matrices de tipo object  //el operador new es el que indica que es herencia del tipo object.
+		personas = new Persona [3][5];
+		
+		nombres = new String [2][3];
+		
+//===================INICIALIZACION DE UNA MATRIZ======================
+//=====================SINTAXIS==========
+		// nombreMatriz [incice_renglon][indice_columna] = valor;
+	//iniciando una matriz de tipo entero(tipo primitivo) 
+		enteros [0][0]= 1;
+		enteros [1][2]= 5;
+	//iniciando una matriz de tipo Object
+		personas [0][0] = new("juan") ;
+	    personas [1][0] = new("pepe","lopez");
+	    personas [1][1] = new ("daniel");
+	}
+
+}
